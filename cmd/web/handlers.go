98632a1e17d8806757d223e5d7474b40a197712a
Mega-rlot <ihamstertop33@gmail.com>
2023-12-19 23:30:28 +0600

    project reorganization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,90 +1,21 @@
 package main
 
 import (
-    //"fmt"
-	"html/template" 
-    "log"
-    "net/http"
-    //"strconv"
+	//"fmt"
+
+	"net/http"
+	//"strconv"
 )
 
-var tpl *template.Template
+func landing(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "landing.html", r)
 
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	files := []string{
-		"./ui/html/home_page.html",
-		"./ui/html/base_layout.html",
-		"./ui/html/footer_partial.html",
-		"./ui/html/header_partial.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
- 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
 }
 
 func forum(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path!= "/forum" {
-        http.NotFound(w, r)
-        return
-    }
+	templates.ExecuteTemplate(w, "forum.html", r)
+}
 
-    files := []string{
-		"./ui/html/forum.html",
-        "./ui/html/base_layout.html",
-        "./ui/html/footer_partial.html",
-        "./ui/html/header_partial.html",
-    }
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
- 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}}
- 
 func adviсe(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path!= "/adviсe" {
-        http.NotFound(w, r)
-        return
-    }
-
-    files := []string{
-		"./ui/html/adviсe.html",
-        "./ui/html/base_layout.html",
-        "./ui/html/footer_partial.html",
-        "./ui/html/header_partial.html",
-    }
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
- 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
+	templates.ExecuteTemplate(w, "adviсe.html", r)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-func main() {
+var templates *template.Template
 
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
+func main() {
+	templates = template.Must(template.ParseGlob("ui/html/*.html"))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", landing)
 	mux.HandleFunc("/forum", forum)
 	mux.HandleFunc("/adviсe", adviсe)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    log.Println("Запуск веб-сервера на http://localhost:4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	log.Println("Запуск веб-сервера на http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
